a333be01e1f5367fae479aa53a4cfc2809725da9
siddh <siddh@Siddhi>
2022-09-06 14:10:21 +0530

    This is Assignment3

---
+++ Prime/src/Prime_btwn_range.java
@@ -0,0 +1,23 @@
+
+public class Prime_btwn_range {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r1 = 100;															// Lower Range Limit
+		int r2 = 200;															// Maximum Range Limit
+		for(int i =r1; i < r2 ;i++) {											// checking the prime no. from 100 to 200
+			int count =0;
+			for(int j=2 ;j < i/2 ;j++ ) {
+				if(i%j == 0)													// if number is composite then the loop breaks
+				{	count++;
+					break;
+				}
+			}
+			if(count == 0) {													// Printing prime numbers.
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
